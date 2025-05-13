d82d24de1402fc71f1888b47581a7ec7b599ca6f
Hamja Sultan <sultanhamja597@gmail.com>
2025-05-13 17:58:16 +0100

    implement front end

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "categories#index"
 
   resources :categories, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
